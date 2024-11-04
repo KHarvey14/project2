3b8b4bf2956daa3703f0e81260377b211475cd31
KHarvey14 <Kayla.harv13@gmail.com>
2024-11-03 19:28:13 -0500

    Add files via upload

--- GamePanel.java
+++ GamePanel.java
@@ -24,6 +24,7 @@ public class GamePanel extends JPanel implements KeyListener {
     }
 
     public GamePanel() {
+        setPreferredSize(new Dimension(600, 600)); // Set preferred size
         playerTank = new PlayerTank(100, 100, 100, 5, "DOWN");
         enemyTank =(new EnemyTank(200, 200, 100, 2, "UP"));
         walls = new ArrayList<>();
@@ -78,14 +79,38 @@ public class GamePanel extends JPanel implements KeyListener {
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-
+        
         //Draw enemy tank with current location
+        Color darkRed = new Color(139, 0, 0); // RGB for dark red
+        Color darkGreen = new Color(0, 100, 0); // RGB for dark green
+
         g.setColor(Color.RED);
-        g.fillRect(enemyTank.getX(),enemyTank.getY(),50,50);
+        g.fillRect(enemyTank.getX(),enemyTank.getY(),40,50); // Body
+        g.setColor(darkRed); 
+        g.fillRect(enemyTank.getX()-10,enemyTank.getY()-5,10,60); // Left wheels
+        g.fillRect(enemyTank.getX()+40,enemyTank.getY()-5,10,60); // Right wheels
+        g.fillRect(enemyTank.getX()+5,enemyTank.getY()+5,30,35); // Medium Box
+        g.setColor(Color.RED);
+        g.fillRect(enemyTank.getX()+11,enemyTank.getY()+10,18,20); // Small Box
+        g.setColor(Color.BLACK);
+        g.fillRect(enemyTank.getX()+17,enemyTank.getY()-20,5,20); // Big shooter
+        g.fillRect(enemyTank.getX()+10,enemyTank.getY()-8,20,8); // Big shooter base
+        g.fillRect(enemyTank.getX()+18,enemyTank.getY()+5,4,8); // Small shooter
+
 
         //Draw player tank with current location
         g.setColor(Color.GREEN);
-        g.fillRect(playerTank.getX(),playerTank.getY(),50,50);
+        g.fillRect(playerTank.getX(),playerTank.getY(),50,50); // Body
+        g.setColor(darkGreen);
+        g.fillRect(playerTank.getX()-10,playerTank.getY()-5,10,60); // Left wheels
+        g.fillRect(playerTank.getX()+40,playerTank.getY()-5,10,60); // Right wheels
+        g.fillRect(playerTank.getX()+5,playerTank.getY()+5,30,35); // Medium Box
+        g.setColor(Color.GREEN);
+        g.fillRect(playerTank.getX()+11,playerTank.getY()+10,18,20); // Small Box
+        g.setColor(Color.BLACK);
+        g.fillRect(playerTank.getX()+17,playerTank.getY()-20,5,20); // Big shooter
+        g.fillRect(playerTank.getX()+10,playerTank.getY()-8,20,8); // Big shooter base
+        g.fillRect(playerTank.getX()+18,playerTank.getY()+5,4,8); // Small shooter
     }
 
     @Override
@@ -96,6 +121,7 @@ public class GamePanel extends JPanel implements KeyListener {
             case KeyEvent.VK_S -> playerTank.setHeading("DOWN");
             case KeyEvent.VK_D -> playerTank.setHeading("RIGHT");
         }
+        repaint();
     }
 
     @Override
